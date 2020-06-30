2fcfe32ac4f6d81d09046f9a88b5464788f15122
Bruno Moura <bruno8moura@gmail.com>
2020-06-30 05:21:05 -0300

    Upload a file.

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,7 +1,11 @@
 import { Router } from 'express';
+import multer from 'multer';
 import CreateUserService from '../services/CreateUserService';
+import ensureUserAuthenticated from '../middlewares/ensureUserAuthenticated';
+import uploadConfig from '../config/upload.files';
 
 const usersRoutes = Router();
+const upload = multer(uploadConfig);
 
 usersRoutes.post('/', async (request, response) => {
     const { name, password, email } = request.body;
@@ -22,5 +26,16 @@ usersRoutes.post('/', async (request, response) => {
         return response.status(400).json({ message: error.message }).end();
     }
 });
+
+usersRoutes.patch(
+    '/avatar',
+    ensureUserAuthenticated,
+    upload.single('avatar'),
+    (request, response) => {
+        console.log(request.file);
+
+        return response.json({ ok: true }).end();
+    },
+);
 
 export default usersRoutes;
