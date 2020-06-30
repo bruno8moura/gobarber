240eeac11a2d65c08d9f39918d571493fa05b748
Bruno Moura <bruno8moura@gmail.com>
2020-06-30 06:03:34 -0300

    Serve static files.

--- src/server.ts
+++ src/server.ts
@@ -3,10 +3,11 @@ import 'reflect-metadata';
 import express from 'express';
 import routes from './routes';
 import './database';
+import uploadConfig from './config/upload.files';
 
 const app = express();
 app.use(express.json());
-
+app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
 app.listen(3333, () => {
     console.log('🚀 >>> Server Start on port 3333 >>> 🚀');
