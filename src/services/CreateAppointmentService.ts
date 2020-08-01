6da7d68f687dd27d57f7450cff63f080870da56f
Bruno Moura <bruno8moura@gmail.com>
2020-08-01 07:33:54 -0300

    Provider validation.
    The providerId must exist before a new appointment to be created.

--- src/services/CreateAppointmentService.ts
+++ src/services/CreateAppointmentService.ts
@@ -3,6 +3,8 @@ import { getCustomRepository } from 'typeorm';
 import Appointment from '../models/Appointment';
 import AppointmentsRepository from '../repositories/AppointmentsRepository';
 import AppError from '../errors/AppError';
+import User from '../models/User';
+import { getRepository } from 'typeorm';
 
 interface Request {
     date: Date;
@@ -12,11 +14,18 @@ interface Request {
 export default class CreateAppointmentService {
     public async execute({ date, providerId }: Request): Promise<Appointment> {
         const repository = getCustomRepository(AppointmentsRepository);
+        const usersRepository = getRepository(User);
         const appointmentDate = startOfHour(date);
 
         const appointmentAlreadyRecorded = await repository.findByDate(
             appointmentDate,
         );
+
+        const providerFound = await usersRepository.findOne({where: {id: providerId}});
+
+        if(!providerFound) {
+            throw new AppError('Provider not found.');
+        }
 
         if (appointmentAlreadyRecorded) {
             throw new AppError('This appointment is already booked.');
